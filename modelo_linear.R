be0f18652731249361e6939b1c792b5cb191da8e
Dalyana Ribeiro <duilianobrega@gmail.com>
2022-08-16 15:45:50 -0300

    Modelo linear

---
+++ modelo_linear.R
@@ -0,0 +1,12 @@
+
+pacman::p_load(dplyr, htmltools, leaflet, leaflet.extras, leaflet.minicharts, leafpop, plotly, rgdal, sf, tidyr)
+
+# Objeto simples
+vetor <-c(1,2,3,5,7)
+
+# Objeto complexo
+# Modelo de regressão
+regCarros <- lm(mpg ~ ., mtcars)
+
+# complexidade do objeto
+str(mtcars)
